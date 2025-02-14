b192beda6b3375ce91048ba82669897437fff2ad
matosluizdev <luizmatosdev@outlook.com>
2025-02-13 22:38:27 -0300

    Exercicio 1 de javascript

---
+++ fundamentos-fullstack/javascript/cadastroRecrutas.js
@@ -0,0 +1,16 @@
+ let firstName
+ let lastName
+ let studyArea
+ let birthYear
+ let age
+
+ firstName = prompt("Qual o seu nome?")
+ lastName = prompt("Qual o seu sobrenome?")
+ studyArea = prompt("Qual a sua área de estudo?")
+ birthYear = prompt("Qual o seu ano de nascimento?")
+ age =  2025 - birthYear
+
+
+ alert(`Olá, ${firstName} ${lastName}! Você tem ou vai fazer ${age()} anos e estuda ${studyArea}.`)
+
+
---
+++ fundamentos-fullstack/javascript/calculadora.js
@@ -0,0 +1,17 @@
+
+ const primeiroNumero = prompt("Digite o primeiro número:")
+ const segundoNumero = prompt("Digite o segundo número:")
+
+ const x = parseFloat(primeiroNumero);
+ const y = parseFloat(segundoNumero);
+
+ const soma = x + y;
+ const subtracao = x - y;
+ const multiplicacao = x * y;
+ const divisao = x / y;
+
+
+
+ alert(`A soma dos números é ${soma} , a subtração é ${subtracao}, a multiplicação é ${multiplicacao} e a divisão é ${divisao}`)
+
+ 
